2650f3213d51464d68295d94a2d11882f4b61bc3
manman4 <manchanr4@gmail.com>
2020-07-06 09:42:53 +0900

    フーリエ変換による畳み込みを用いた多項式の乗算(1) Numo::Pocketfftをgemコマンドでインストールしておく。

---
+++ 200301_01/200301_01.rb
@@ -0,0 +1,40 @@
+require 'numo/pocketfft'
+include Numo
+
+def A(k, n)
+  ary = []
+  x = Int32[1]
+  a = Int32[*[1] * k]
+  (n + 1).times{
+    ary << x.to_a
+    # Numo::PocketfftはDFloat
+    x = Int32.cast(Numo::Pocketfft.fftconvolve(x, a).map{|i| i.round})
+  }
+  ary.flatten
+end
+
+def B(k, n)
+  ary = []
+  x = Int64[1]
+  a = Int64[1..k]
+  (n + 1).times{
+    ary << x.to_a
+    # Numo::PocketfftはDFloat
+    x = Int64.cast(Numo::Pocketfft.fftconvolve(x, a).map{|i| i.round})
+  }
+  ary.flatten
+end
+
+def A329708(n)
+  ary = []
+  (0..n).each{|i|
+    a = Int32[1..i + 1]
+    # Numo::PocketfftはDFloat
+    ary << Int32.cast(Numo::Pocketfft.fftconvolve(a, a).map{|i| i.round}).to_a
+  }
+  ary.flatten
+end
+
+(1..5).each{|i| p A(i, 10)}
+(1..5).each{|i| p B(i, 10)}
+p A329708(10)
