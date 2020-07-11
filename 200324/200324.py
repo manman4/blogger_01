4a2b6d85be5b0086047c0b3e3cf1bb95b3cc66eb
manman4 <manchanr4@gmail.com>
2020-07-11 18:56:16 +0900

    A140517等

---
+++ 200324/200324.py
@@ -0,0 +1,23 @@
+from graphillion import GraphSet
+import graphillion.tutorial as tl
+
+def A(points, n):
+    if n == 1: return 1
+    universe = tl.grid(n - 1, n - 1)
+    GraphSet.set_universe(universe)
+    cycles = GraphSet.cycles()
+    for i in points:
+        cycles = cycles.including(i)
+    return cycles.len()
+
+print([A([                            ], n) for n in range(2, 10)])
+
+print([A([1                           ], n) for n in range(2, 10)])
+
+print([A([1, n                        ], n) for n in range(2, 10)])
+print([A([1,    n * (n - 1) + 1       ], n) for n in range(2, 10)])
+print([A([1,                     n * n], n) for n in range(2, 10)])
+
+print([A([1, n, n * (n - 1) + 1       ], n) for n in range(2, 10)])
+
+print([A([1, n, n * (n - 1) + 1, n * n], n) for n in range(2, 10)])
