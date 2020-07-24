2ab2bd914026f7e0b1d3132554cbca103bc50276
manman4 <manchanr4@gmail.com>
2020-07-25 02:26:59 +0900

    Martin Gardner のlucky number について

---
+++ 200725/200725.rb
@@ -0,0 +1,31 @@
+def A(digit)
+  ary = []
+  (10 ** (digit - 1)..10 ** digit - 1).each{|i|
+    flag = true
+    (1..10 - digit).each{|j|
+      a = (j..j + digit - 1).to_a
+      k = a.join.to_i
+      l = i + k
+      if l.to_s.split('').map(&:to_i).sort != a
+        flag = false
+        break
+      end
+    }
+    ary << i if flag
+  }
+  ary
+end
+
+def B(ary, digit)
+  p ary
+  puts
+  ary.each{|i|
+    (1..10 - digit).each{|j|
+      k = (j..j + digit - 1).to_a.join.to_i
+      puts "#{i} + #{k} = #{i + k}"
+    }
+    puts "-" * (digit * 3 + 6)
+  }
+end
+
+(1..5).each{|i| B(A(i), i)}
