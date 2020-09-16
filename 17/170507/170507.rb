9ed521f39dad3ca58c8b5741e14c131edb2fc833
manman4 <manchanr4@gmail.com>
2020-09-17 00:19:04 +0900

    Number of representations of 10^n as sum of k triangular numbers k が6, 8 のときに規則性がある

---
+++ 17/170507/170507.rb
@@ -0,0 +1,19 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| 2 * s(2, i) - s(1, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - k * a[j] * ary[-j]} / i}
+  ary
+end
+
+m = 3
+n = 10 ** m
+(1..100).each{|i|
+  a = A(i, n)
+  p [i, (0..m).map{|j| a[10 ** j]}]
+}
