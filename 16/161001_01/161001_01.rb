228db9dce1f28a1d18323a70fc8f4b593ec69af8
manman4 <manchanr4@gmail.com>
2020-09-18 14:47:14 +0900

    y^2 + y = x^3 - x^2, y^2 + y = x^3 - x^2 - 10x - 20, y^2 + y = x^3 - x^2 - 7820x - 263580

---
+++ 16/161001_01/161001_01.rb
@@ -0,0 +1,17 @@
+require 'prime'
+
+def A(a3, a2, a4, a6, n)
+  ary = []
+  Prime.take(n).each{|p|
+    a = Array.new(p, 0)
+    (0..p - 1).each{|i| a[(i * i + a3 * i) % p] += 1}
+    ary << (0..p - 1).inject(0){|s, i| s + a[(i * i * i + a2 * i * i + a4 * i + a6) % p]}
+  }
+  ary
+end
+
+n = 500
+p ary = A(1, -1, 0, 0, n)
+p ary == A(1, -1,   -10,     -20, n)
+p ary == A(1, -1, -7820, -263580, n)
+
