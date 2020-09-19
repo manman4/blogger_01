526f0f92eba377aadb9e86b73ea92ff243005810
manman4 <manchanr4@gmail.com>
2020-09-20 02:01:54 +0900

    足し算と引き算だけで円を描く

---
+++ 20/200920/200920.rb
@@ -0,0 +1,12 @@
+def A(r)
+  xy = Array.new(r + 1){Array.new(r + 1, '□')}
+  a, x, y = r, 0, r
+  while x <= y
+    xy[x][y], xy[y][x] = '■', '■'
+    a, x = a - x - x - 1, x + 1
+    a, y = a + y + y - 1, y - 1 if a < 0
+  end
+  xy.each{|i| p i.join}
+end
+
+(0..20).each{|i| A(i)}
