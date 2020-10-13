8abec8011d2816d9d57ba7f0b4ecac563f0aa6cd
manman4 <manchanr4@gmail.com>
2020-10-13 23:29:22 +0900

    Product_{k >= 1} (1 - x^k)^m の計算

---
+++ 17/170328/170328.rb
@@ -0,0 +1,16 @@
+def s(n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - k * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 15
+-24.upto(24){|i| p [i, A(i, n)]}
+
