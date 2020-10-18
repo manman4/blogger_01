ef7c24eb2b89ef1fc77d35340fb7cf08adebd2a6
manman4 <manchanr4@gmail.com>
2020-10-18 16:29:17 +0900

    A166243 Villegas-Zagier polynomials

---
+++ 18/180805/180805.rb
@@ -0,0 +1,34 @@
+def d(ary)
+  s = ary.size - 1
+  return [0] if s == 0
+  (1..s).map{|i| i * ary[i]}
+end
+
+def e(ary)
+  while ary[-1] == 0
+    ary.pop
+  end
+  ary
+end
+
+def A(n)
+  a, b = [0], [1]
+  cnt = -1
+  p [cnt, a]
+  while cnt < n
+    cnt += 1
+    s, t = a.size - 1, b.size - 1
+    c0 = d(b)
+    c = [0] * 3 + c0.map{|i| i * 8}
+    (0..t - 1).each{|i| c[i] -= c0[i]}
+    u = 16 * cnt + 3
+    (0..t).each{|i| c[i + 2] -= b[i] * u}
+    v = 4 * cnt * (2 * cnt - 1)
+    (0..s).each{|i| c[i + 1] -= a[i] * v}
+    a, b = b, e(c)
+    p [cnt, a]
+  end
+end
+
+A(3 * 10)
+
