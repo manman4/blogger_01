60d46c58bbe6b476b87d3d609d9f51e2a90c6e86
manman4 <manchanr4@gmail.com>
2020-10-22 15:36:03 +0900

    ラマヌジャンのタウ函数のいろいろな計算方法(1)

---
+++ 17/171224/171224_01_01.rb
@@ -0,0 +1,33 @@
+# m次以下を取り出す
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# m次以下を取り出す
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(n)
+  ary = [1]
+  (1..n).each{|i|
+    b_ary = Array.new(i + 1, 0)
+    b_ary[0], b_ary[-1] = 1, -1
+    ary = mul(ary, b_ary, n)
+  }
+  power(ary, 24, n)
+end
+
+n = 19
+p A(n - 1)
