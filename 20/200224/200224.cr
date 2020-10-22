f86c68d07ddb236538f852eff0a5ae0f149160bb
manman4 <manchanr4@gmail.com>
2020-10-22 21:05:29 +0900

    A320843(2)

---
+++ 20/200224/200224.cr
@@ -0,0 +1,37 @@
+class Count
+  @@cnt = 0
+  def initialize
+    @@cnt += 1
+  end
+  def self.instances
+    @@cnt
+  end
+  def self.to_zero
+    @@cnt = 0
+  end
+end
+
+def search(a, num, n)
+  if num == n + 1
+    Count.new
+  else
+    (1..n).each{|i|
+      if a[i] == 0
+        if num % i == 0 || i % num == 0
+          a[i] = num
+          search(a, num + 1, n)
+          a[i] = 0
+        end
+      end
+    }
+  end
+end
+
+def a320843(n)
+  a = [0] * (n + 1)
+  Count.to_zero
+  search(a, 1, n)
+  Count.instances
+end
+
+p (0..20).map{|i| a320843(i)}
