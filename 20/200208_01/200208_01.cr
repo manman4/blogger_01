34422f07607ded0bab2043daa0e94edeb70dfb3d
manman4 <manchanr4@gmail.com>
2020-10-22 21:19:49 +0900

    フィボナッチ数列(2)

---
+++ 20/200208_01/200208_01.cr
@@ -0,0 +1,22 @@
+require "big"
+
+struct BigInt
+  def add!(other : BigInt) : BigInt
+    LibGMP.add(self, self, other)
+    self
+  end
+end
+
+def fib(n)
+  a = 0.to_big_i
+  b = 1.to_big_i
+  n.times{|i|
+    a.add!(b)
+    a, b = b, a
+    puts "#{i + 1} : #{a}"
+  }
+end
+
+time = Time.local
+fib(100)
+puts Time.local - time
