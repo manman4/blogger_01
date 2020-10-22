8a6e5b1363d09e5ce8888acbca1a05e257beda91
manman4 <manchanr4@gmail.com>
2020-10-22 15:39:10 +0900

    ラマヌジャンのタウ函数のいろいろな計算方法(1) {-24, -24, -24, ... } のEuler transform を利用する。

---
+++ 17/171224/171224_01_03.rb
@@ -0,0 +1,15 @@
+def s(n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0}
+  s
+end
+
+def A(m, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - m * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 19
+p A(24, n - 1)
