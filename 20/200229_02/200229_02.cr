b846899e7c987c0a69445565028074424efb0c60
manman4 <manchanr4@gmail.com>
2020-10-22 20:46:35 +0900

    A332800(2)

---
+++ 20/200229_02/200229_02.cr
@@ -0,0 +1,27 @@
+def a332800_conjecture(n)
+  return 1 if n == 0
+  a = (1..n).map{|i| [i]}
+  len = 1
+  while len < n
+    b = Array(Array(Int32)).new
+    a.each{|c|
+      (1..n).each{|num|
+        if !c.includes?(num)
+          i = c.clone + [num]
+          if len > 1
+            if (i[-3] % i[-2]) < (i[-2] % i[-1])
+              b << i
+            end
+          else
+            b << i
+          end
+        end
+      }
+    }
+    a = b
+    len += 1
+  end
+  a.size
+end
+
+(0..19).each{|i| p a332800_conjecture(i)}
