034c872afc635ea3c470c9d7007bdb87d631fbda
manman4 <manchanr4@gmail.com>
2020-11-03 00:07:26 +0900

    異なる単位分数（< 1）の和が1 より大きい自然数(2)

---
+++ 16/160106_03/160106_03.rb
@@ -0,0 +1,14 @@
+# 和因子はmin以上max以下で互いに異なる
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i - 1).map{|rest| [i, *rest]}}
+end
+
+(2..135).each{|n|
+  p_ary = []
+  partition(n, 2, n).each{|ary|
+    a = ary.inject(0){|s, i| s + 1 / i.to_r}
+    p_ary << [a.numerator, ary] if a.denominator == 1
+  }
+  p [n, p_ary]
+}
