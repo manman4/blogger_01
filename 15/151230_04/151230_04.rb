cd5840c91af68dd00f350482b4a18a94066f24ef
manman4 <manchanr4@gmail.com>
2020-11-11 09:27:43 +0900

    p(pn | 和因子は素数)(2)

---
+++ 15/151230_04/151230_04.rb
@@ -0,0 +1,18 @@
+require 'prime'
+
+def A056768(n)
+  ary = Prime.each.take(n)
+  m = ary[-1]
+  ps = Array.new(m + 1){0}
+  ps[0] = 1
+  a_ary = []
+  ary.each{|num|
+    (num..m).each{|i|
+      ps[i] += ps[i - num]
+    }
+    a_ary << ps[num]
+  }
+  a_ary
+end
+
+p A056768(10 ** 3)
